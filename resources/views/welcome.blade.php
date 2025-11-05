f5046c2c84cab49b3bb2843e0f939e073b9d90df
AirtonBorges <airtonborges2211@gmail.com>
2025-11-04 21:58:25 -0300

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Laravel</h1>
+    <h2>Pontos fortes</h2>
+    <li>gera um template full stack</li>
+    <li>curva de aprendisado rápida</li>
+    <li>boa experiencia de desenvolvimento</li>
+</body>
+</html>
